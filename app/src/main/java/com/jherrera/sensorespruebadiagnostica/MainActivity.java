389c0240dc15fffe7148d19935dd8177ccb459cf
jherrera245 <245jh17@gmail.com>
2023-01-21 19:12:01 -0600

    Creacion y configurancion de main activity

--- app/src/main/java/com/jherrera/sensorespruebadiagnostica/MainActivity.java
+++ app/src/main/java/com/jherrera/sensorespruebadiagnostica/MainActivity.java
@@ -3,12 +3,24 @@ package com.jherrera.sensorespruebadiagnostica;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button buttonProximidad;
+    private Button buttonLuminosidad;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setInitComponents();
+    }
+
+    //inicializar elemeentos de la ui
+    private void setInitComponents() {
+        buttonProximidad = findViewById(R.id.buttonProximidad);
+        buttonLuminosidad = findViewById(R.id.buttonLuminosidad);
     }
 }
